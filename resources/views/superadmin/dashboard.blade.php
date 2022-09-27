4dfc071e864e87d08aaf9331d27e26d75947d290
shadi alrwas <92518108+shadisco22@users.noreply.github.com>
2022-09-27 18:34:21 +0300

    add lis in notification

--- resources/views/superadmin/dashboard.blade.php
+++ resources/views/superadmin/dashboard.blade.php
@@ -7,10 +7,16 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    list of notifications
-                </div>
+            <div class="bg-white rounded-lg border border-gray-200 w-full text-gray-900">
+                <a href="#!" aria-current="true"
+                    class="block px-6 py-2 border-b border-gray-200 w-full rounded-t-lg bg-blue-600 text-white cursor-pointer">
+                    The current link item
+                </a>
+                <a href="#!"
+                    class="block px-6 py-2 border-b border-gray-200 w-full hover:bg-gray-100 hover:text-gray-500
+                    focus:outline-none focus:ring-0 focus:bg-gray-200 focus:text-gray-600 transition duration-500 cursor-pointer">
+                    A second link item
+                </a>
             </div>
         </div>
     </div>
